6f9c1d9648b0f0436e0b359c240cfe573f09c2fd
Michael <mmuench27@gmail.com>
2022-07-29 10:56:25 +0200

    new structure for navigation

--- json_to_markdown.py
+++ json_to_markdown.py
@@ -16,129 +16,106 @@ def MkOutputDir(vendor_dict):
                 return output_dir['model']
             else:
                 os.mkdir(output_dir["model"])
-                Path(output_dir['model']/"_index.md").touch()
         else:
             os.mkdir(output_dir["device_class"])
             Path(output_dir["device_class"]/"_index.md").touch()
             os.mkdir(output_dir["model"])
-            Path(output_dir['model']/"_index.md").touch()
     else:
         os.mkdir(output_dir["vendor"])
         Path(output_dir['vendor']/"_index.md").touch()
         os.mkdir(output_dir["device_class"])
         Path(output_dir['device_class']/"_index.md").touch()
         os.mkdir(output_dir["model"])
-        Path(output_dir['model']/"_index.md").touch()
 
     return output_dir['model']
 
 
-def TestIfMarkdownExist(vendor_name, version, dir):
-    file_exists = os.path.exists(f'{dir}/{vendor_name}{version}.md')
+def TestIfMarkdownExist(device_name, dir):
+    file_exists = os.path.exists(f'{dir}/{device_name}.md')
 
     if not file_exists:
-        os.mknod(f'{dir}/{vendor_name}{version}.md')
+        os.mknod(f'{dir}/{device_name}.md')
+        return False
+
+    return True
 
 
-def KeywordInFile(keyword,vendor,version,dir):
+def KeywordInFile(keyword,device,dir):
     try:
-        f1 = open(f'{dir}/{vendor}{version}.md','r')
+        f1 = open(f'{dir}/{device}.md','r')
         lines = f1.readlines()
         f1.close()
         keyword_in_md = False
         for line in lines:
             if keyword in line:
                 keyword_in_md = True
+                print(f"keyword: {keyword}")
+                print(line)
+                return keyword_in_md
 
         return keyword_in_md
 
     except BaseException as err:
         print(f'(Error: {err})')
-    
 
 
-def ReplaceLine(old,vendor, dir):
-    # print(f'replace: {old}')
+def WriteVendor(vendor, model, dir):
     try:
-        f1 = open(f'{dir}/{vendor}.md','r')
-        lines = f1.readlines()
-        f1.close()
+        f1 = open(f'{dir}/{model}.md','w')
+        f1.write(f'\n# {vendor}\n')
+        f1.close()  
     except BaseException as err:
         print(f'(Error: {err})')
 
-    for line in lines:
-        if old in line:
-            line.replace("uptodate", "old")
+
+def WriteDeviceClass(device_class, model, dir): 
+    
+    try:
+        f1 = open(f'{dir}/{model}.md','at')
+        f1.write(f"\n## {device_class}\n")
+        f1.close()  
+    except BaseException as err:
+        print(f'(Error: {err})')
+
+
+def WriteDevice(device, model, dir):
+    # if model is in MD:
+    print(f'{device["model"]}')
+    if KeywordInFile(f'{device["model"]}',model, dir):
+        # if firmware version of model is in MD:
+        print(f'|{device["model"]}|{device["version"]}')
+        if KeywordInFile(f'|{device["model"]}|{device["version"]}', model, dir):
+            print("device already in list")
+        # if just another version of the firmware is in the MD
+        else:
+            print("ELSE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            filepath = f'[{device["filepath"]}](../../../../firmware_files/{device["filepath"]})'
+            url = f'[{device["filepath"]}]({device["url"]})'
+            
             try:
-                f1 = open(f'{dir}/{vendor}.md','w')
-                f1.write(lines)
+                f1 = open(f'{dir}/{model}.md','a')
+                print("SCHREIB!!!!!!!111111111111111111111111111111111111111111111111")
+                f1.write(f'|{device["model"]}|{device["version"]}|{device["date"]}|{url}|{filepath}|{device["checksum"]}|\n')
                 f1.close()
             except BaseException as err:
                 print(f'(Error: {err})')
 
 
-def WriteVendor(vendor,model,version, dir):
-   if KeywordInFile(f'# {vendor}',model,version, dir):
-        print("Vendor already excists")
-   else:
-        try:
-            f1 = open(f'{dir}/{model}{version}.md','w')
-            f1.write(f'\n# {vendor}\n')
-            f1.close()  
-        except BaseException as err:
-            print(f'(Error: {err})')
-
-
-def WriteDeviceClass(device_class, model, version, dir): 
-    if KeywordInFile(f'## {device_class}',model,version, dir):
-        print('device class already excists')
+    # if model is NOT in MD:
     else:
-        try:
-            f1 = open(f'{dir}/{model}{version}.md','at')
-            f1.write(f"\n## {device_class}\n")
-            f1.write(f"\n|modell|version|date|url|filepath|checksum|status|")
-            f1.write(f"\n|:---:|:---:|:---:|:---:|:---:|:---:|:---:|\n")
-            f1.close()  
-        except BaseException as err:
-            print(f'(Error: {err})')
-
-
-def WriteDevice(device, model, version, dir):
-    if KeywordInFile(f'{device["model"]}',model,version, dir):
-        if KeywordInFile(f'{device["model"]} |{device["version"]}',model,version, dir):
-            print("device already in list")
-        else:
-            ReplaceLine(f'{device["model"]}|',model, dir)
-    else:
-        try:
-            f1 = open(f'{dir}/{model}{version}.md','r')
-            lines = f1.readlines()
-            f1.close()
-        except BaseException as err:
-            print(f'(Error: {err})')
-
-        for line in lines:
-            if device["device_class"] in line:
-                position_of_deviceclass = lines.index(line)
-
+        print("ELSE")
         filepath = f'[{device["filepath"]}](../../../../firmware_files/{device["filepath"]})'
         url = f'[{device["filepath"]}]({device["url"]})'
 
-        lines.insert(position_of_deviceclass+4, f'|{device["model"]} |')  
-        lines.insert(position_of_deviceclass+5, f'{device["version"]} |')
-        lines.insert(position_of_deviceclass+6, f'{device["date"]} |')
-        lines.insert(position_of_deviceclass+7, f'{url} |')
-        lines.insert(position_of_deviceclass+8, f'{filepath} |')
-        lines.insert(position_of_deviceclass+9, f'{device["checksum"]} |')
-        lines.insert(position_of_deviceclass+10, f'{device["status"]} |\n')
-
         try:
-            f1 = open(f'{dir}/{model}{version}.md','w')
-            f1.writelines(lines)       
-            f1.close()
-
+            f1 = open(f'{dir}/{model}.md','at')
+            f1.write(f"\n|modell|version|date|url|filepath|checksum|")
+            f1.write(f"\n|:---:|:---:|:---:|:---:|:---:|:---:|\n")
+            f1.write(f'|{device["model"]}|{device["version"]}|{device["date"]}|{url}|{filepath}|{device["checksum"]}|\n')
+            f1.close()  
         except BaseException as err:
             print(f'(Error: {err})')
+       
 
 ## Start
 # parser = argparse.ArgumentParser()
@@ -148,7 +125,7 @@ def WriteDevice(device, model, version, dir):
 # input_vendor = args.vendor
 # input_vendor = input_vendor.lower()
 
-input_vendor = "tplink"
+input_vendor = "zyxel"
 
 try:
     with open(f'router_json/{input_vendor}.json', 'r') as read_file:
@@ -171,12 +148,13 @@ for devices in device_json_list:
     device["url"] = devices['files'][0]['url']
     device["filepath"] = devices['files'][0]['path']
     device["checksum"] = devices['files'][0]['checksum']
-    device["status"] = devices['files'][0]['status']
+    # device["status"] = devices['files'][0]['status']
 
     output_dir = MkOutputDir(device)
 
-    TestIfMarkdownExist(device["model"], device['version'], output_dir)
-    
-    WriteVendor(device["vendor"],device["model"], device['version'],output_dir)
-    WriteDeviceClass(device["device_class"],device["model"], device['version'] , output_dir)
-    WriteDevice(device, device["model"], device['version'],output_dir)    
+    mdExisted= TestIfMarkdownExist(device["model"], output_dir)
+
+    if not mdExisted:
+        WriteVendor(device["vendor"],device["model"],output_dir)
+        WriteDeviceClass(device["device_class"],device["model"], output_dir)
+    WriteDevice(device, device["model"],output_dir)    
